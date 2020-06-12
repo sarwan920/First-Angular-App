37407307ca65eb93273bc5d474981623d2e78772
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2020-06-11 19:57:27 -0700

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessButtonComponent } from './success-button/success-button.component';
+import { PrimaryAlertComponent } from './primary-alert/primary-alert.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    SuccessButtonComponent,
+    PrimaryAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/primary-alert/primary-alert.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-primary-alert',
+    templateUrl: './primary-alert.component.html',
+    styleUrls: ['./primary-alert.component.css']
+
+})
+
+export class PrimaryAlertComponent{
+
+}
